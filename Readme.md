fdc8e52c44cbace007b5dc5ca81a7bccd0d39416
davidblaqq <ikukoyidave@gmail.com>
2023-01-10 14:48:11 +0100

    team jobber

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Rookie-jobber
+
+This is a job application created by a team of nerds for the purpose of bridging the gap between the unemployed and recruiters
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="App">
+     <h1>Rookie jobber app</h1>
+     <h2>This is my first time creating a team github repository so kindly forgive any issues you might face</h2>
+     <p>This is going to be a bumpy ride</p>
+    </div>
+  )
+}
+
+export default App
